823ef3f7611f9549cbaa0f35406d9cf0f99c0160
kianroz122 <kianroz122@gmail.com>
2021-12-01 20:41:26 +0000

    Razor Part 2

--- Models/Student.cs
+++ Models/Student.cs
@@ -1,16 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 
 namespace ASP_RazorContoso.Models
 {
     public class Student
     {
         public int StudentID { get; set; }
+        [DisplayName("Last Name")]
         public string LastName { get; set; }
+        [DisplayName("First Name")]
         public string FirstName { get; set; }
+        [DisplayName("Enrol Date")]
         public DateTime EnrollmentDate { get; set; }
 
         // Relationships or navigation properties
         public virtual ICollection<Enrollment> Enrollments { get; set; }
+
+        public string FullName()
+        {
+            return FirstName + "" + LastName;
+        }
     }
 }
